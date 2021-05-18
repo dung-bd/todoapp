0cbc6847f63b1e978a38ad70700e26d91ae82f85
dung <dungbachdang123@gmail.com>
2021-05-18 08:57:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, {useState} from "react"
 import TodoList from "./TodoList"
 import TodoForm from "./TodoForm"
+import './App.scss'
+class Task {
+  constructor(id,name,isComplete){
+      this.id=id
+      this.name=name
+      this.isComplete=isComplete
+  }
+  complete(){
+    this.isComplete=true
+  }
+  setName(name){
+    this.name=name
+  }
+}
 function App(){
   const[todoList, setTodoList] = useState([])
   function handleRemoveClick(todo){
     const newTodoList = todoList.filter((x)=>x.id !== todo.id)
     setTodoList(newTodoList)
   }
+  function handleComplete(id){
+    setTodoList((state)=>{
+        return state.map((todo)=>{
+            if(todo.id === id){
+              todo.complete()
+            
+              
+            }
+            return todo
+          })
+           
+    })
+  }
+ function handleUpdate(){
+
+ }
   function handleAddForm(formValues){
     console.log("Form submit: ", formValues)
-    const newTodo={
-      id: todoList.length + 1,
-      ... formValues
-    }
+    const newTodo=new Task(todoList.length + 1,formValues.title,false)
     const newTodoList = [... todoList]
     newTodoList.push(newTodo)
     setTodoList(newTodoList)
   }
     return(
-      <div>
+      <div className="App">
         <h1>Welcome to my todoApp</h1>
         <TodoForm onSubmit={handleAddForm}/>
-        <TodoList todos={todoList} onTodoClick={handleRemoveClick} />
+        <p>doing</p>
+        <TodoList todos={todoList.filter(e=>!e.isComplete)} onTodoClick={handleRemoveClick} handleComplete={handleComplete} />
+        <p>complete</p>
+        <TodoList todos={todoList.filter(e=>e.isComplete)} onTodoClick={handleRemoveClick} />
       </div>
     )
 }
--- src/TodoList/index.jsx
+++ src/TodoList/index.jsx
@@ -1,15 +1,14 @@
-import React from "react"
-import PropTypes from "prop-types"
-TodoList.propTypes={
-    todos: PropTypes.array,
-    onTodoClick: PropTypes.func
-}
-TodoList.defaultProps={
-    todos:[],
-    onTodoClick: null
-}
+import React,{useCallback} from "react"
+
+
 function TodoList(props){
-    const{todos, onTodoClick} = props
+    const{todos, onTodoClick,handleComplete} = props
+    
+    // const handleClick=useCallback((todo)=>{
+    //     if(onTodoClick){
+    //         onTodoClick(todo)
+    //     }
+    // },[onTodoClick])
     function handleClick(todo){
         if(onTodoClick){
             onTodoClick(todo)
@@ -18,12 +17,14 @@ function TodoList(props){
     return(
         <ul className="todo-list">
             {todos.map(todo=>(
-                <div>
-                <li key={todo.id}>{todo.title}</li>
+                <div className="todo-list-item">
+                <li key={todo.id}>{todo.name}</li>
                 <button onClick={() => handleClick(todo)}>Delete</button>
+                <button onClick={()=>{handleComplete(todo.id)}}>complete</button>
                 </div>
             ))}
         </ul>
     )
 }
+
 export default TodoList
